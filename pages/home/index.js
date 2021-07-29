6adcb0f3f5bf6bb1b0519755e83ff3a5f8420981
Vincent Vic <vcoding@dingtalk.com>
2021-07-29 12:31:00 +0800

    发现页面分类实现V0.0.01

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,15 +1,40 @@
 Page({
     data: {
         tabs:['全部服务','在提供','正在找'],
-        currentTabIndex:0
+        currentTabIndex:0,
+        categoryList:[
+            {
+                id:1,
+                name:'疏通'
+            },
+            {
+                id:2,
+                name:'家电维修'
+            },
+            {
+                id:3,
+                name:'在线教育'
+            },
+            {
+                id:4,
+                name:'婚恋'
+            },
+            {
+                id:5,
+                name:'宠物'
+            }
+        ]
     },
     onLoad: function (options) {
 
     },
     handlerTabChange:function (e){
-        console.log(e)
+        console.log(e);
         this.setData({
             currentTabIndex:e.currentTarget.dataset.index
         })
+    },
+    handlerCategoryChange:function (e){
+        console.log(e);
     }
 });
