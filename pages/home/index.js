dbba87ab32eaa47ba50a6321653e3ac02d71dd71
Vincent Vic <vcoding@dingtalk.com>
2021-07-29 10:56:06 +0800

    发现页面tab实现V0.0.01

--- pages/home/index.js
+++ pages/home/index.js
@@ -1,6 +1,15 @@
 Page({
-    data: {},
+    data: {
+        tabs:['全部服务','在提供','正在找'],
+        currentTabIndex:0
+    },
     onLoad: function (options) {
 
+    },
+    handlerTabChange:function (e){
+        console.log(e)
+        this.setData({
+            currentTabIndex:e.currentTarget.dataset.index
+        })
     }
 });
