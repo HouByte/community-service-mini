f54fe39b368ff37b0655e2bd68705ee644138862
Vincent Vic <vcoding@dingtalk.com>
2021-08-01 16:47:56 +0800

    使用外部ui组件完成下单成功页面

--- pages/order-success/index.js
+++ pages/order-success/index.js
@@ -1,6 +1,18 @@
+import roleType from "../../enum/role-type";
+
 Page({
     data: {},
     onLoad: function (options) {
 
+    },
+    handleCheckOrder:function (){
+        wx.redirectTo({
+            url:`/pages/my-order/index?role=${roleType.COUSUMER}&status=${orderStatus.UNAPPROVED}`
+        })
+    },
+    handleNavToHome:function (){
+        wx.switchTab({
+            url:'/pages/home/index'
+        })
     }
 });
