8ab81b65c13667df6b10c881a1652f63f841789b
Vincent Vic <vcoding@dingtalk.com>
2021-08-04 21:28:36 +0800

    订单预览和订单信息组件&信息通用组件

---
+++ components/info-item/info-item.js
@@ -0,0 +1,20 @@
+Component({
+    properties: {
+        title:String,
+        content:String,
+        titleColor:{
+            type:String,
+            value:"#4F4B4BFF"
+        },
+        contentColor:{
+            type:String,
+            value:"#4F4B4BFF"
+        },
+        size:{
+            type:String,
+            value:"34"
+        }
+    },
+    data: {},
+    methods: {}
+});
